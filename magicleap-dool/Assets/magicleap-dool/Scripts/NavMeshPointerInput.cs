e2a51924aa1c1317c2e1f10aee581a8c34fbcc03
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2020-09-27 06:20:13 +0900

    NavMeshに移動する座標を設定するためのスクリプトの追加及びシーンの調整

---
+++ magicleap-dool/Assets/magicleap-dool/Scripts/NavMeshPointerInput.cs
@@ -0,0 +1,55 @@
+﻿using MagicLeapTools;
+using UnityEngine;
+
+
+namespace MagicLeapDool
+{
+    /// <summary>
+    /// MagicLeap-ToolKitのControlPointerを利用して指定した座標にNavMeshAgentを移動するようにする.
+    /// </summary>
+    public class NavMeshPointerInput : MonoBehaviour
+    {
+
+        [SerializeField] Pointer pointer;
+        [SerializeField] MoveToClickPoint moveToClickPoint;
+        
+
+        /// <summary>
+        /// Trigger押下処理,指定した座標にAgentが移動するようにしている.
+        /// </summary>
+        public void OnTriggerDown()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(new Ray(pointer.Origin, pointer.Direction), out hit))
+            {
+                moveToClickPoint.OnInputClicked(hit.point);
+            }
+        }
+
+
+        /// <summary>
+        /// Bumperボタン押下処理.
+        /// 指定した座標にAgentを配置しなおすために利用.
+        /// </summary>
+        public void OnBumperButtonDown()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(new Ray(pointer.Origin, pointer.Direction), out hit))
+            {
+                moveToClickPoint.SetPosition(hit.point);
+            }
+        }
+
+
+        /// <summary>
+        /// HomeButton押下処理.
+        /// 実機動作時にアプリを終了させるため.
+        /// </summary>
+        public void OnHomeButtonDown()
+        {
+            Application.Quit();
+        }
+
+    }
+
+}
